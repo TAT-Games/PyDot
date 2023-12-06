cc623a28f9de1b1db3df5ff356c9827cb40eda7e
TAT-Games <tatgames2023@gmail.com>
2023-12-06 11:21:19 -1100

    Improved printErr function
    I updated it to print the correct line an error occurred/where it was called

--- GlobalScope/functions.py
+++ GlobalScope/functions.py
@@ -1,24 +1,16 @@
 from pygame import Color
 from inspect import currentframe, stack
 
-def printErr(error: str, ErrorType: str = "ERROR", help: str = "", print_source: bool = True):
+def printErr(error: str, ErrorType: str = "ERROR", help: str = "", print_source: bool = True) -> None:
     """Print an error to the terminal"""
     stack_list: list = stack()
-    caller_line = currentframe().f_back.f_lineno
-    caller_frame = stack_list[1]
-    filename = caller_frame[0].f_code.co_filename
-    
-    line = caller_line
-    
-    if len(stack_list) >= 3:
-        calling_frame = stack_list[2]
-        calling_function_line = calling_frame[0].f_lineno
-        line = calling_function_line
-    
-    
+    calling_function_line = currentframe().f_back.f_lineno
+    calling_frame = stack_list[len(stack_list) - 1]
+    calling_function_filename = calling_frame[0].f_code.co_filename
+
     print(f"\n{ErrorType}: {error}")
     if print_source:
-        print(f"Source: Line {line} in \"{filename}\"")
+        print(f"Source: Line {calling_function_line} in \"{calling_function_filename}\"")
         
     if help != "":
         print(f"Help: {help}")
--- GlobalScope/scene.py
+++
@@ -1,8 +0,0 @@
-import sys; sys.path.append(".")
-from GlobalScope.node import Node
-
-def Scene(node: Node):
-    """Transforms a node to a scene"""
-    node.owner = node
-    node.__is_scene = True
-    return node
